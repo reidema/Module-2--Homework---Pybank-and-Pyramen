912258ac2e2d1d515735cf812e4d8b63e0f3f490
reidema <117589787+reidema@users.noreply.github.com>
2023-04-21 12:27:26 -0400

    commit

---
+++ PyRamen/.ipynb_checkpoints/PyRamen-checkpoint.py
@@ -0,0 +1,109 @@
+# -*- coding: UTF-8 -*-
+"""PyRamen Homework Starter."""
+
+# @TODO: Import libraries
+import csv
+from pathlib import Path
+
+# @TODO: Set file paths for menu_data.csv and sales_data.csv
+menu_filepath = Path(r'C:\Users\reide\Downloads\Module-2--Homework---Pybank-and-Pyramen\PyRamen\Resources\menu_data.csv')
+sales_filepath = Path(r'C:\Users\reide\Downloads\Module-2--Homework---Pybank-and-Pyramen\PyRamen\Resources\sales_data.csv')
+
+# @TODO: Initialize list objects to hold our menu and sales data
+menu = []
+sales = []
+
+# @TODO: Read in the menu data into the menu list
+
+with open (menu_filepath, 'r') as csvfile:
+    csvmenu_reader = csv.reader(menu_filepath, delimeter = ',')
+    
+    menu_header = next(csvmenu_reader)
+    line_num += 1
+    
+    print(menu_header)
+
+
+
+
+
+
+
+# @TODO: Read in the sales data into the sales list
+
+
+
+
+
+
+
+
+
+
+# @TODO: Initialize dict object to hold our key-value pairs of items and metrics
+report = {}
+
+# Initialize a row counter variable
+row_count = 0
+
+# @TODO: Loop over every row in the sales list object
+
+
+
+
+    # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
+    # @TODO: Initialize sales data variables
+
+
+    # @TODO:
+    # If the item value not in the report, add it as a new entry with initialized metrics
+    # Naming convention allows the keys to be ordered in logical fashion, count, revenue, cost, profit
+
+
+
+
+
+
+
+
+    # @TODO: For every row in our sales data, loop over the menu records to determine a match
+
+
+        # Item,Category,Description,Price,Cost
+        # @TODO: Initialize menu data variables
+
+
+
+
+        # @TODO: Calculate profit of each item in the menu data
+
+
+        # @TODO: If the item value in our sales data is equal to the any of the items in the menu, then begin tracking metrics for that item
+
+
+            # @TODO: Print out matching menu data
+
+
+
+
+
+
+            # @TODO: Cumulatively add up the metrics for each item key
+
+
+
+
+
+        # @TODO: Else, the sales item does not equal any fo the item in the menu data, therefore no match
+
+
+
+    # @TODO: Increment the row counter by 1
+
+
+# @TODO: Print total number of records in sales data
+
+
+
+
+# @TODO: Write out report to a text file (won't appear on the command line output)
